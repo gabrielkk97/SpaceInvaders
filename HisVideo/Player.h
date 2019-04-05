ec04badea27ef1cc84f6b734736bf74d822e6108
noobgag <longsang.kong@hotmail.com>
2019-04-05 22:52:47 +0800

    Refactored the classes

--- HisVideo/Player.h
+++ HisVideo/Player.h
@@ -4,14 +4,12 @@
 class Bullet;
 class Player {
 public:
-	int x;
-	int y;
-	int hp = 100;
+
+	
 	int playerSize = 50;
 	float shotdelay = 1.f;
 
 	int currentNode = 0;
-	int currentBullet = 0;
 	int MaxBullet = 20;
 	float moveSpeed = 5.f;
 
@@ -23,12 +21,30 @@ public:
 	   //Mx and My are the change of position of the player , eg if Mx  =1 then the player is moved to the right for one pixel
 	   void Onprint(float Mx,float My, HDC hdc);
 
-	   void setMod(int x_, int y_);
+
+	   int getX()const;
+	   int getY()const;
 	   int getMX() const;
 	   int getMy() const;
+	   int getCurrentBullet()const;
+
+	   
+	   void setX(int x_);
+	   void setY(int y_);
+	   void setMod(int x_, int y_);
+	   void setCurrentBullet(int number);
+
+
+	   
+	
 
 private:
+	int x;
+	int y;
 	int ModX = 0;
 	int ModY = 0;
+
+	int currentBullet = 0;
+	int hp = 100;
 	HDC m_HDC;
 };
